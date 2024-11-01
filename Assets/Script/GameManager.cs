c2dc71bc0b64a53ef5c7013521d79fc5175638a6
popoyo07 <piwedaa@gmail.com>
2024-10-31 21:58:16 -0400

    Created Game manager object, player object and enemy 2 object.
    enemy 2 still need polish because right now it is spawning outside the screen.

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -6,11 +6,13 @@ public class GameManager : MonoBehaviour
 {
     public GameObject player;
     public GameObject enemy;
+    public GameObject enemy2; // Enemy2 object will be attached here si it can spawn 
     // Start is called before the first frame update
     void Start()
     {
         Instantiate(player, transform.position, Quaternion.identity);
         InvokeRepeating("CreateEnemy", 1f, 3f);
+        InvokeRepeating("CreateEnemy2", 3f, 1f);
     }
     // Update is called once per frame
     void Update()
@@ -20,5 +22,14 @@ public class GameManager : MonoBehaviour
     {
         Instantiate(enemy, new Vector3(Random.Range(-9f, 9f), 8f, 0),
         Quaternion.identity);
+
+
+    }
+
+    void CreateEnemy2()
+    {
+        Instantiate(enemy2, new Vector3(Random.Range(-9f, 9f), 8f, 0),
+        Quaternion.identity);
+
     }
 }
