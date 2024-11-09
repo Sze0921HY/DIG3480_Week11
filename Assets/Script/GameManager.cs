fdec11b94491a37924dc881760fcd385b68f07bb
Sze0921HY <melloyeung0921@knights.ucf.edu>
2024-11-09 17:57:12 -0500

    Same Setup as Zoom

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,36 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+
     public GameObject player;
     public GameObject enemy;
-    public GameObject enemy2; // Enemy2 object will be attached here si it can spawn 
+    public GameObject cloud;
+    private int score;
+
+    public TextMeshProUGUI scoreText;
+
     // Start is called before the first frame update
     void Start()
     {
         Instantiate(player, transform.position, Quaternion.identity);
         InvokeRepeating("CreateEnemy", 1f, 3f);
-        InvokeRepeating("CreateEnemy2", 1f, 1f);
+        CreateSky();
+        score = 0;
+        scoreText.text = "Score: " + score;
     }
+
     // Update is called once per frame
     void Update()
     {
+
     }
+
     void CreateEnemy()
     {
-        Instantiate(enemy, new Vector3(Random.Range(-9f, 9f), 8f, 0),
-        Quaternion.identity);
-
-
+        Instantiate(enemy, new Vector3(Random.Range(-9f, 9f), 7.5f, 0), Quaternion.identity);
     }
 
-    void CreateEnemy2()
+    void CreateSky()
     {
-        // they spawn at the edge of the scrren at the right
-        Instantiate(enemy2, new Vector3(11.5f, Random.Range(-6f, 0.5f), 0),
-        Quaternion.identity);
-
+        for (int i = 0; i < 30; i++)
+        {
+            Instantiate(cloud, transform.position, Quaternion.identity);
+        }
     }
+
+    public void EarnScore(int howMuch)
+    {
+        score = score + howMuch;
+        scoreText.text = "Score: " + score;
+    }
+
 }
